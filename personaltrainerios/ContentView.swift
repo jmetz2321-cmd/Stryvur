8ba224a68a826c9f48f9859e6b92dec652f3ef8a
Jon <jmetz2321@gmail.com>
2026-05-07 11:23:10 -0700

    Initial Commit

---
+++ personaltrainerios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  personaltrainerios
+//
+//  Created by Jon Metz on 5/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ personaltrainerios/personaltraineriosApp.swift
@@ -0,0 +1,17 @@
+//
+//  personaltraineriosApp.swift
+//  personaltrainerios
+//
+//  Created by Jon Metz on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct personaltraineriosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
